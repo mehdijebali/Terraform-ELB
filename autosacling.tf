2ac8ed5fc5321c1216f3907d165efc15d4d1843b
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-09-14 09:00:45 +0100

    Edit autoscaling resource file

--- autosacling.tf
+++ autosacling.tf
@@ -1,9 +1,9 @@
 #AutoScaling Launch Configuration
-resource "aws_launch_configuration" "levelup-launchconfig" {
-  name_prefix     = "levelup-launchconfig"
-  image_id        = lookup(var.AMIS, var.AWS_REGION)
-  instance_type   = "t2.micro"
-  key_name        = aws_key_pair.levelup_key.key_name
+resource "aws_launch_configuration" "tf-launchconfig" {
+  name_prefix     = var.LAUNCH_CONFIGURATION_NAME
+  image_id        = var.AMI_ID
+  instance_type   = var.INSTANCE_TYPE
+  key_name        = aws_key_pair.tf-ssh-key.key_name
   security_groups = [aws_security_group.levelup-instance.id]
   user_data       = "#!/bin/bash\napt-get update\napt-get -y install net-tools nginx\nMYIP=`ifconfig | grep -E '(inet 10)|(addr:10)' | awk '{ print $2 }' | cut -d ':' -f2`\necho 'Hello Team\nThis is my IP: '$MYIP > /var/www/html/index.html"
 
@@ -13,26 +13,26 @@ resource "aws_launch_configuration" "levelup-launchconfig" {
 }
 
 #Generate Key
-resource "aws_key_pair" "levelup_key" {
-    key_name = "levelup_key"
+resource "aws_key_pair" "tf-ssh-key" {
+    key_name = var.KEY_NAME
     public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
 #Autoscaling Group
 resource "aws_autoscaling_group" "levelup-autoscaling" {
-  name                      = "levelup-autoscaling"
+  name                      = var.AUTOSCALING_GROUP_NAME
   vpc_zone_identifier       = [aws_subnet.levelupvpc-public-1.id, aws_subnet.levelupvpc-public-2.id]
-  launch_configuration      = aws_launch_configuration.levelup-launchconfig.name
-  min_size                  = 2
-  max_size                  = 2
-  health_check_grace_period = 200
-  health_check_type         = "ELB"
+  launch_configuration      = aws_launch_configuration.tf-launchconfig.name
+  min_size                  = var.AUTOSCALING_GROUP_MIN_SIZE
+  max_size                  = var.AUTOSCALING_GROUP_MAX_SIZE
+  health_check_grace_period = var.HEALTH_CHECK_GRACE_PERIOD
+  health_check_type         = var.HEALTH_CHECK_TYPE
   load_balancers            = [aws_elb.levelup-elb.name]
   force_delete              = true
 
   tag {
     key                 = "Name"
-    value               = "LevelUp Custom EC2 instance via LB"
+    value               = "EC2 instance via LB Autoscaling"
     propagate_at_launch = true
   }
 }
