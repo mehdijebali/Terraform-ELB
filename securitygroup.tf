b35c898066d04a0a0817b907a78e663c305a6fa8
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-09-14 09:14:29 +0100

    Create sf resource file

---
+++ securitygroup.tf
@@ -0,0 +1,56 @@
+#Security group for AWS ELB
+resource "aws_security_group" "elb-securitygroup" {
+  vpc_id      = aws_vpc.levelupvpc.id
+  name        = "elb-sg"
+  description = "security group for Elastic Load Balancer"
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "elb-sg"
+  }
+}
+
+#Security group for the Instances
+resource "aws_security_group" "instance-securitygroup" {
+  vpc_id      = aws_vpc.levelupvpc.id
+  name        = var.INSTANCE_SG_NAME
+  description = "security group for instances"
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.elb-securitygroup.id]
+  }
+
+  tags = {
+    Name = var.INSTANCE_SG_NAME
+  }
+}
