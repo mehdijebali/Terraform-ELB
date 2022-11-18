6286b793c2dc29c7f0d396f7ad29e89008a6c85d
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-11-18 16:20:29 +0100

    Add user data for linux2

--- installnginx.sh
+++ installnginx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update
-apt-get -y install net-tools nginx
+yum update
+yum -y install net-tools nginx
 MYIP=`ifconfig | grep -E '(inet 10)|(addr:10)' | awk '{ print $2 }' | cut -d ':' -f2`
 echo 'Hello Team, this is my IP: '$MYIP > /var/www/html/index.html
