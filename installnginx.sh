a7545d6a262aafe8cbe3c43057f9bfd6b1c959e8
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-11-21 15:32:51 +0100

    Edit user data file

--- installnginx.sh
+++ installnginx.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 yum update
-yum -y install net-tools nginx
+amazon-linux-extras enable nginx1
+yum clean metadata
+yum install -y net-tools nginx
+systemctl start nginx
+systemctl enable nginx
 MYIP=`ifconfig | grep -E '(inet 10)|(addr:10)' | awk '{ print $2 }' | cut -d ':' -f2`
 echo 'Hello Team, this is my IP: '$MYIP > /var/www/html/index.html
