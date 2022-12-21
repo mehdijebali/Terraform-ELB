2674b4352b0bf0055f061cdf74cdb249754d44d8
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-12-21 11:14:06 +0100

    Update variables

--- variables.tf
+++ variables.tf
@@ -65,3 +65,34 @@ variable "INSTANCE_SG_DESCRIPTION" {
 variable "ELB_SG_DESCRIPTION" {
   default = "security group for Elastic Load Balancer"
 }
+
+variable "VPC_NAME" {
+  default = "demo-vpc"
+}
+
+variable "VPC_CIDR_BLOCK" {
+  default = "10.0.0.0/16"
+}
+
+variable "GW_NAME" {
+  default = "demo-gw"
+}
+
+variable "PUBLIC_RT_NAME" {
+  default = "demo-rt"
+}
+
+variable "AVAILABILITY_ZONES" {
+   type = list
+   default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "SUBNET_NAMES" {
+   type = list
+   default = ["public-subnet-A", "public-subnet-B", "private-subnet-A", "private-subnet-B"]
+}
+
+variable "SUBNET_IPS" {
+   type = list
+   default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
+}
