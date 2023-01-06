28754847a1fcd31519efe57230715e6369e3883d
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-01-06 15:24:12 +0100

    Retrieve packer ami

--- main.tf
+++ main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region     = var.AWS_REGION
 }
 
+data "aws_ami" "packer_ami" {
+  most_recent      = true
+  owners           = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["custom-configuration-*"]
+  }
+}
+
 module "network" {
     source      = "./modules/network"
 
