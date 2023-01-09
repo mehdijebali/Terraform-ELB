226fa30743991e4d33e37eb5b6df573257eda2de
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-01-09 14:15:04 +0100

    Update shell script

--- installnginx.sh
+++ installnginx.sh
@@ -2,4 +2,4 @@
 apt-get update
 apt-get -y install nginx net-tools
 MYIP=`ifconfig | grep -E '(inet 10)|(addr:10)' | awk '{ print $2 }' | cut -d ':' -f2`
-echo 'Hello Team, this is my IP: ${MYIP}' > /var/www/html/index.html
+echo 'Hello Team, this is my IP:' $MYIP > /var/www/html/index.html
