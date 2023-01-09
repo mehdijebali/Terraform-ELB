27314f683511bd80a8cce4acc6ddcaadec1d4d89
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-01-09 12:46:15 +0100

    install net-tools

--- installnginx.sh
+++ installnginx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update
-apt-get -y install nginx
+apt-get -y install nginx net-tools
 MYIP= $(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 #echo 'Hello Team, this is my IP: ${MYIP}' > /var/www/html/index.html
