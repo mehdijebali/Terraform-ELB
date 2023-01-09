797d5638ec80d9d579b29978e3822e7fabda5f81
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-01-09 12:37:46 +0100

    debug ip variable

--- installnginx.sh
+++ installnginx.sh
@@ -2,4 +2,4 @@
 apt-get update
 apt-get -y install nginx
 MYIP= $(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-echo 'Hello Team, this is my IP: ${MYIP}' > /var/www/html/index.html
+#echo 'Hello Team, this is my IP: ${MYIP}' > /var/www/html/index.html
