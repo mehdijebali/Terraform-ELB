14b977318cb532239e7e99d7d1153df7a515814f
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-01-09 14:35:41 +0100

    Generate random number

--- installnginx.sh
+++ installnginx.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 apt-get update
-apt-get -y install nginx net-tools
-MYIP=`ifconfig | grep -E '(inet 10)|(addr:10)' | awk '{ print $2 }' | cut -d ':' -f2`
-echo 'Hello Team, this is my IP:' $MYIP > /var/www/html/index.html
+apt-get -y install nginx 
+echo 'Hello Team, this is my random number:' $$ > /var/www/html/index.html
