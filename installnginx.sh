84bcb05f9c1850761b7086f1c523a19cef47e5d4
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-01-10 16:11:41 +0100

    fix index.html writing

--- installnginx.sh
+++ installnginx.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-apt-get update
-apt-get -y install nginx net-tools
+
+# Update packages
+sudo yum update -y
+
+# Install nginx
+sudo amazon-linux-extras enable nginx1
+sudo yum clean metadata
+sudo yum -y install nginx
+sudo systemctl enable nginx
+
+# Install net-tools
+sudo yum -y install net-tools.x86_64
