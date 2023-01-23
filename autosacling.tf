39b1f4da70be31edab65c5047ec920ed6813a66b
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-01-23 14:12:46 +0100

    Set default version as latest version

--- autosacling.tf
+++ autosacling.tf
@@ -6,6 +6,7 @@ resource "aws_launch_template" "tf-launchtemplate" {
   key_name = aws_key_pair.tf-ssh-key.key_name
   vpc_security_group_ids = [aws_security_group.instance-securitygroup.id]
   user_data = filebase64("configurenginx.sh")
+  update_default_version = true
 }
 
 #Generate Key
