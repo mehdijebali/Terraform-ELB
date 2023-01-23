dda843ad7fd88135094d2a716f09b6481c6ab103
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-01-23 14:15:54 +0100

    format tf files

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region     = var.AWS_REGION
+  region = var.AWS_REGION
 }
 
 data "aws_ami" "packer_ami" {
-  most_recent      = true
-  owners           = ["self"]
+  most_recent = true
+  owners      = ["self"]
 
   filter {
     name   = "name"
@@ -13,13 +13,13 @@ data "aws_ami" "packer_ami" {
 }
 
 module "network" {
-    source      = "./modules/network"
+  source = "./modules/network"
 
-    VPC_NAME = var.VPC_NAME
-    VPC_CIDR_BLOCK  = var.VPC_CIDR_BLOCK
-    GW_NAME = var.GW_NAME
-    PUBLIC_RT_NAME = var.PUBLIC_RT_NAME
-    AVAILABILITY_ZONES = var.AVAILABILITY_ZONES
-    SUBNET_NAMES = var.SUBNET_NAMES
-    SUBNET_IPS = var.SUBNET_IPS
+  VPC_NAME           = var.VPC_NAME
+  VPC_CIDR_BLOCK     = var.VPC_CIDR_BLOCK
+  GW_NAME            = var.GW_NAME
+  PUBLIC_RT_NAME     = var.PUBLIC_RT_NAME
+  AVAILABILITY_ZONES = var.AVAILABILITY_ZONES
+  SUBNET_NAMES       = var.SUBNET_NAMES
+  SUBNET_IPS         = var.SUBNET_IPS
 }
