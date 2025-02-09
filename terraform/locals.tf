a9af48de3f39d44653dbbb0c7fae3a031fe50470
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2025-02-09 22:35:20 +0100

    ✨ Modify conditions with local variables

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,6 +3,7 @@ locals {
   ASG_NAME         = "EC2 instance via LB Autoscaling"
   INSTANCE_SG_NAME = "instance-sg"
   ELB_SG_NAME      = "elb-sg"
+  ami-name         = var.LD_NAME == "centos" ? "centos-template-pkr-*" : "ubuntu-template-pkr-*"
 
   elb-tags = {
     Name = local.ELB_NAME
