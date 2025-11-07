3158c1c6f548019cb4a3e74cf040998e5c626f6e
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2025-11-07 09:46:35 +0100

    📝 Feat/Set user data condition as local

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -4,6 +4,7 @@ locals {
   INSTANCE_SG_NAME = "instance-sg"
   ELB_SG_NAME      = "elb-sg"
   ami-name         = var.LD_NAME == "centos" ? "al2023-template-pkr-*" : "ubuntu-template-pkr-*"
+  template-user-data = var.LD_NAME == "centos" ? file("./config/configurenginx_centos.sh") : file("./config/configurenginx_ubuntu.sh")
   elb-tags = {
     Name = local.ELB_NAME
   }
