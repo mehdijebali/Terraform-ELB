020b9da046aa9766e00893b7fa279a3024021cef
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2025-11-27 16:53:33 +0100

    🐛 Fix/format tf files

--- terraform/cloud-init.tf
+++ terraform/cloud-init.tf
@@ -1,18 +1,18 @@
 data "cloudinit_config" "elb_instance_userdata" {
-  gzip            = true
-  base64_encode   = true 
+  gzip          = true
+  base64_encode = true
 
   # Part 1: Shell script for initial setup
   part {
-    filename      = "Configure Nginx"
-    content_type  = "text/x-shellscript"
-    content       = local.template-user-data
+    filename     = "Configure Nginx"
+    content_type = "text/x-shellscript"
+    content      = local.template-user-data
   }
 
   # Part 2: Shell script for ssm agent installation
   part {
-    filename      = "Install SSM Agent"
-    content_type  = "text/x-shellscript"
-    content       = module.ssm-role.user_data
+    filename     = "Install SSM Agent"
+    content_type = "text/x-shellscript"
+    content      = module.ssm-role.user_data
   }
 }
